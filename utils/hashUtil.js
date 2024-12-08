99e1d2b840ce0499c25161c2e3d25e64b7fc122d
Rahul <yadavrahulm071162@gmail.com>
2024-12-08 12:39:15 +0530

    ..

--- utils/hashUtil.js
+++ utils/hashUtil.js
@@ -1,12 +1,25 @@
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
 
-const hashPassword = async (password) => {
-    const saltRounds = 10;
-    return await bcrypt.hash(password, saltRounds);
+// const hashPassword = async (password) => {
+//     const saltRounds = 10;
+//     return await bcrypt.hash(password, saltRounds);
+// };
+
+// const comparePassword = async (password, hashedPassword) => {
+//     return await bcrypt.compare(password, hashedPassword);
+// };
+
+// module.exports = { hashPassword, comparePassword };
+
+
+const storePassword = (password) => {
+    // Store the password directly (not secure)
+    return password;
 };
 
-const comparePassword = async (password, hashedPassword) => {
-    return await bcrypt.compare(password, hashedPassword);
+const comparePassword = (password, storedPassword) => {
+    // Compare the plaintext password directly
+    return password === storedPassword;
 };
 
-module.exports = { hashPassword, comparePassword };
+module.exports = { storePassword, comparePassword };
